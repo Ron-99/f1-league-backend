3eea6215605179e42ac15197e9ecd84c9051ed13
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-09 14:50:29 -0300

    Porta da aplicaçao alterada

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@
 const app = require('../src/app');
 const http = require('http');
 
-const port = normalizePort(process.env.PORT || '4000')
+const port = normalizePort(process.env.PORT || '8080')
 app.set('port', port);
 
 const server = http.createServer(app);
--- makefile
+++ makefile
@@ -3,14 +3,14 @@ include .env
 .PHONY: up
 
 up:
-	sudo docker-compose up -d
+	docker-compose up -d
 
 .PHONY: down
 
 down:
-	sudo docker-compose down
+	docker-compose down
 
 .PHONY: logs
 
 logs:
-	sudo docker-compose logs -f
+	docker-compose logs -f
