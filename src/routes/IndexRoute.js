f19b832208f9ad8c0db94c61568910d18f65dcd0
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-12 08:44:31 -0300

    Versao atualizada

--- src/routes/IndexRoute.js
+++ src/routes/IndexRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (_, res) => {
     res.status(200).send({
         title: 'API da Liga do Brasil',
-        version: '1.0.1',
+        version: '1.0.2',
         author: 'Ronaldo Gomes',
         github: 'github.com/ron-99'
     });
