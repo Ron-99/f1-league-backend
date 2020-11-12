2492aadd8d3c521bf5dc587dbeaf19f7b50c31e8
Ronaldo Gomes <ronaldosg1999@outlook.com>
2020-11-12 10:32:29 -0300

    Descriçao atualizada

--- src/routes/IndexRoute.js
+++ src/routes/IndexRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', (_, res) => {
     res.status(200).send({
         title: 'API da e-racebrasil',
+        description: 'Essa API tem como intuito a criação de ligas para corridas de vários jogos',
         version: '1.0.2',
         author: 'Ronaldo Gomes',
         github: 'github.com/ron-99',
